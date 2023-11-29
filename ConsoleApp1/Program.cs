a71ec7f46839cb5ad2b4a9c00f19ddb294d7e4d2
Kitesalet <104630744+Kitesalet@users.noreply.github.com>
2023-11-29 16:40:19 -0300

    Modificado

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -25,6 +25,7 @@ class Program
 
         //SerilogExceptions();
 
+
         //NLog
 
         //NLog();
@@ -33,6 +34,7 @@ class Program
         //log4net
 
         //Log4net();
+
 
         //LoggerManual
 
